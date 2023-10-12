90fd499fe6d1ccf235df4462a89e644b3234d3e6
vgishyan <vahagn.gishyan.a@gmail.com>
2023-10-12 15:39:10 +0400

    feat[pp-test]: adds database tests

--- database/database.py
+++ database/database.py
@@ -11,21 +11,22 @@ class Database:
         new_table = db.Table(table_name)  # Assuming you have a Table class
         self.tables.append(new_table)
 
+    # def drop_table(self, table_name):
+    #     table = self.get_table(table_name)
+    #     self.tables.remove(table)
+
     def drop_table(self, table_name):
-        # Find and remove the specified table from the database
-        table_to_drop = next((table for table in self.tables if table.name == table_name), None)
-        if table_to_drop:
-            self.tables.remove(table_to_drop)
-        else:
-            raise ValueError(f"Table '{table_name}' not found in the database.")
+        # Get the table by name
+        table = self.get_table(table_name)
+        # Remove the table from the list
+        self.tables.remove(table)
 
     def get_table(self, table_name):
-        # Retrieve a table from the database by its name
-        table = next((table for table in self.tables if table.name == table_name), None)
-        if table:
-            return table
-        else:
-            raise ValueError(f"Table '{table_name}' not found in the database.")
+        # Retrieve a table from the database by its name using a for loop
+        for table in self.tables:
+            if table.name == table_name:
+                return table
+        raise ValueError(f"Table '{table_name}' not found in the database.")
 
     def list_tables(self):
         # Return a list of table names in the database
