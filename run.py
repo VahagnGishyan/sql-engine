29c3ee8c5c47872e0fe3f02f959d8f470d8bb5c0
vgishyan <vahagn.gishyan.a@gmail.com>
2023-10-12 17:15:10 +0400

    feat[pp-v0.2.0]: first db for to-do-list

--- run.py
+++ run.py
@@ -1,27 +1,61 @@
 
-from database import constraints as dbcstrs
-from database import column as db
 
-def create_column_with_constraints(name, data_type, constraints=None):
-    return db.Column(name, data_type, constraints)
+# Import the necessary modules
+from database import manager as dbm
+from database import database as db
+from database import table as tbl
+from database import column as col
+from database import constraints as cstr
 
-def check_func(column, element_value, element_type):
-    return (element_value > 0)
+#############################################################
+#                                                           #
+#############################################################
 
-constraint_check = dbcstrs.ConstraintCheck(check_func)
-my_column = create_column_with_constraints("my_column", "int", [constraint_check])
+# Step 0: Set data names
+db_name = "ToDoListDB"
+table_name = "ToDoList"
 
-# Add some elements
-my_column.add_element(10, "int")
-my_column.add_element(20, "int")
-my_column.add_element(30, "int")
+#############################################################
+#                                                           #
+#############################################################
 
-# Remove elements by value
-my_column.remove_element(20)
+# Step 1: Create a database
+todo_db = db.Database(dbm.create_database(db_name))
 
-# Add another element
-my_column.add_element(40, "int")
 
-# Print the remaining elements
-for element in my_column.elements:
-    print(f"Value: {element.value}, Type: {element.type}")
+# Step 2: Create a table for the to-do list tasks
+todo_table = todo_db.create_table(table_name)
+
+# Step 3: Define columns for the to-do list tasks
+todo_table.add_column("Name", "text", [cstr.ConstraintNotNull()])
+todo_table.add_column("Description", "text", [cstr.ConstraintNotNull()])
+todo_table.add_column("DueDate", "date", [cstr.ConstraintNotNull()])
+todo_table.add_column("Completed", "boolean", [
+    cstr.ConstraintDefault(False)])
+
+# Step 4: Add data to columns
+taskList = [
+    [("Name", "Task 1"), ("Description", "Description 1"),
+     ("DueDate", "2023-10-15"), ("Completed", False)],
+    [("Name", "Task 2"), ("Description", "Description 2"),
+     ("DueDate", "2023-10-16"), ("Completed", True)],
+    [("Name", "Task 3"), ("Description", "Description 3"),
+     ("DueDate", "2023-10-17"), ("Completed", False)],
+    [("Name", "Task 4"), ("Description", "Description 4"),
+     ("DueDate", "2023-10-18"), ("Completed", True)],
+]
+for task in taskList:
+    todo_table.insert_data(task)
+
+print(f"Table: {todo_table.name}")
+
+# Print column headers
+column_headers = [column.name for column in todo_table.columns]
+for header in column_headers:
+    print(f"{header} ",  end="")
+print()
+
+# Print rows
+for row in todo_table.get_rows():
+    row_values = [str(element.value) for element in row]
+    print(f"{row_values}")
