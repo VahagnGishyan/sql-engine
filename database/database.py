d99135d9b927693823b153209ef075349a966875
vgishyan <vahagn.gishyan.a@gmail.com>
2023-10-12 15:01:37 +0400

    feat[pp-db]: adds database

---
+++ database/database.py
@@ -0,0 +1,46 @@
+ 
+from database import table as db
+
+class Database:
+    def __init__(self, name):
+        self.name = name
+        self.tables = []
+
+    def create_table(self, table_name):
+        # Create a new table and add it to the database
+        new_table = db.Table(table_name)  # Assuming you have a Table class
+        self.tables.append(new_table)
+
+    def drop_table(self, table_name):
+        # Find and remove the specified table from the database
+        table_to_drop = next((table for table in self.tables if table.name == table_name), None)
+        if table_to_drop:
+            self.tables.remove(table_to_drop)
+        else:
+            raise ValueError(f"Table '{table_name}' not found in the database.")
+
+    def get_table(self, table_name):
+        # Retrieve a table from the database by its name
+        table = next((table for table in self.tables if table.name == table_name), None)
+        if table:
+            return table
+        else:
+            raise ValueError(f"Table '{table_name}' not found in the database.")
+
+    def list_tables(self):
+        # Return a list of table names in the database
+        return [table.name for table in self.tables]
+
+    def table_exists(self, table_name):
+        # Check if a table with the given name exists in the database
+        return table_name in self.list_tables()
+
+    def describe_table(self, table_name):
+        # Describe the structure of a table
+        table = self.get_table(table_name)
+        if table:
+            print(f"Table: {table_name}")
+            for column in table.columns:
+                print(f"Column: {column.name}, Type: {column.type}")
+        else:
+            print(f"Table '{table_name}' not found in the database.")
