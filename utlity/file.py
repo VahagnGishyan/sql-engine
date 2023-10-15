61f63cfee7ee0b7c3171dd012bc0fe1762f52cbc
vgishyan <vahagn.gishyan.a@gmail.com>
2023-10-15 13:59:28 +0400

    feat[pp-utility]: adds ut. functions

--- utlity/file.py
+++ utlity/file.py
@@ -8,33 +8,88 @@ import shutil
 #                                                           #
 #############################################################
 
-def ReadFileToList(file_path):
+def read_file_to_list(file_path):
+    # Read and return cleaned lines from a file. 
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
-        # Remove leading and trailing whitespace from each line
         cleaned_lines = [line.strip() for line in lines]
         return cleaned_lines
     except FileNotFoundError:
-        console.PrintWarning(f"File '{file_path}' not found.")
+        console.print_warning(f"File '{file_path}' not found.")
         return []
 
-
-def CopyingFiles(srcdir, destdir):
-    if not os.path.exists(srcdir):
-        console.PrintWarning(
-            f"directory: {srcdir} does not exists")
+def copy_files(src_dir, dest_dir):
+    # Copy files and directories from source to destination. 
+    if not os.path.exists(src_dir):
+        console.print_warning(f"Directory: {src_dir} does not exist.")
         return
-    shutil.copytree(srcdir, destdir, dirs_exist_ok=True)
+    shutil.copytree(src_dir, dest_dir, dirs_exist_ok=True)
 
-
-def CopyFileToFile(fromfile, tofile):
-    sfile = open(fromfile, "r")
-    dfile = open(tofile, "w")
-    for line in sfile:
-        dfile.write(line)
+def copy_file_to_file(from_file, to_file):
+    # Copy the contents of one file to another.
+    s_file = open(from_file, "r")
+    d_file = open(to_file, "w")
+    for line in s_file:
+        d_file.write(line)
 
 
 #############################################################
 #                                                           #
 #############################################################
+
+def is_path_exists(path):
+    # Check if a path exists.
+    return path is not None and os.path.exists(path)
+
+def assert_path_exists(path):
+    # Assert that a path exists or raise a ValueError.
+    if not is_path_exists(path):
+        raise ValueError(f"'{path}' does not exist.")
+
+def is_file(path):
+    # Check if a path is a file.
+    return os.path.isfile(path)
+
+def is_dir(path):
+    # Check if a path is a directory.
+    return os.path.isdir(path)
+
+def assert_is_file(path):
+    # Assert that a path is a file or raise a ValueError.
+    if not is_file(path):
+        raise ValueError(f"'{path}' is not a file.")
+
+def assert_is_dir(path):
+    # Assert that a path is a directory or raise a ValueError.
+    if not is_dir(path):
+        raise ValueError(f"'{path}' is not a directory.")
+
+def list_dirs_in_dir(path):
+    # List directories in a given directory.
+    return [d for d in os.listdir(path) if is_dir(os.path.join(path, d))]
+
+def list_files_in_dir(path):
+    # List files in a given directory.
+    return [f for f in os.listdir(path) if is_file(os.path.join(path, f))]
+
+def list_dir(path):
+    # List names of all files and directories in a given directory.
+    return os.listdir(path)
+
+def is_dir_empty(path):
+    # Check if a directory is empty.
+    return not list_dir(path)
+
+def assert_dir_empty(path):
+    # Assert that a directory is empty or raise a ValueError.
+    if not is_dir_empty(path):
+        raise ValueError(f"'{path}' is not an empty directory.")
+
+
+#############################################################
+#                                                           #
+#############################################################
+
+
+
