01284cca40f1d1d66b83417edc31eb21e7fc0bd5
vgishyan <vahagn.gishyan.a@gmail.com>
2023-10-18 15:20:17 +0400

    fix[pp-tests]: updates tests

--- tests/sqlinterp/operations/test_insert_where.py
+++ tests/sqlinterp/operations/test_insert_where.py
@@ -3,10 +3,7 @@ import unittest
 from database.database import Database
 from database.table import Table
 from database.column import Column, ColumnElement
-from sqlinterp.operations import Select
-from sqlinterp.conditions import ConditionExecutor
-from sqlinterp.conditions import Equal, NotEqual
-from sqlinterp.conditions import GreaterThan, GreaterThanOrEqualTo, LessThanOrEqualTo
+from sqlinterp.operations import InsertInto
 
 
 class TestInsert(unittest.TestCase):
@@ -39,78 +36,6 @@ class TestInsert(unittest.TestCase):
         ]
         for row in data_to_insert:
             self.table.insert_data(row)
-
-    def test_select_specific_columns(self):
-        # Test for selecting specific columns with a condition
-        query = Select(["Name", "City"], [
-            ConditionExecutor("Name", Equal("John"))
-        ])
-        result = query.execute(self.table)
-        # Assert the result based on the condition
-        self.assertEqual(len(result.get_rows()), 1)
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[0].value, "John")
-        self.assertEqual(result.get_column_by_name(
-            "City").elements[0].value, "New York")
-
-    def test_select_age_greater_than_30(self):
-        # Test for selecting rows with Age greater than 30
-        query = Select(["Name", "Age"], [
-            ConditionExecutor("Age", GreaterThan(30))
-        ])
-        result = query.execute(self.table)
-        # Assert the result based on the condition
-        self.assertEqual(len(result.get_rows()), 1)
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[0].value, "Bob")
-        self.assertEqual(result.get_column_by_name(
-            "Age").elements[0].value, 35)
-
-    def test_select_completed_tasks(self):
-        # Test for selecting rows where Completed is True
-        query = Select(["Name", "Completed"], [
-            ConditionExecutor("Completed", Equal(True))
-        ])
-        result = query.execute(self.table)
-        # Assert the result based on the condition
-        self.assertEqual(len(result.get_rows()), 2)
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[0].value, "John")
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[1].value, "Bob")
-        self.assertEqual(result.get_column_by_name(
-            "Completed").elements[0].value, True)
-        self.assertEqual(result.get_column_by_name(
-            "Completed").elements[1].value, True)
-
-    def test_select_age_between_25_and_35(self):
-        # Test for selecting rows where Age is between 25 and 35
-        query = Select(["Name", "Age"], [
-            ConditionExecutor("Age", GreaterThanOrEqualTo(25)),
-            ConditionExecutor("Age", LessThanOrEqualTo(35)),
-        ])
-        result = query.execute(self.table)
-        # Assert the result based on the condition
-        self.assertEqual(len(result.get_rows()), 3)
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[0].value, "John")
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[1].value, "Alice")
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[2].value, "Bob")
-
-    def test_select_city_not_chicago(self):
-        # Test for selecting rows where City is not "Chicago"
-        query = Select(["Name", "City"], [
-            ConditionExecutor("City", NotEqual("Chicago"))
-        ])
-        result = query.execute(self.table)
-        # Assert the result based on the condition
-        self.assertEqual(len(result.get_rows()), 2)
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[0].value, "John")
-        self.assertEqual(result.get_column_by_name(
-            "Name").elements[1].value, "Alice")
 
 
 if __name__ == '__main__':
--- tests/sqlinterp/operations/test_select_where.py
+++ tests/sqlinterp/operations/test_select_where.py
@@ -4,7 +4,9 @@ from database.database import Database
 from database.table import Table
 from database.column import Column, ColumnElement
 from sqlinterp.operations import Select
-from sqlinterp.conditions import Equal, ConditionExecutor, GreaterThan
+from sqlinterp.conditions import ConditionExecutor
+from sqlinterp.conditions import Equal, NotEqual
+from sqlinterp.conditions import GreaterThan, GreaterThanOrEqualTo, LessThanOrEqualTo
 
 
 class TestSelect(unittest.TestCase):
@@ -86,6 +88,78 @@ class TestSelect(unittest.TestCase):
             "Completed").elements[0].get_value(), True)
         self.assertEqual(result.get_column_by_name(
             "Completed").elements[1].get_value(), True)
+
+    def test_select_specific_columns(self):
+        # Test for selecting specific columns with a condition
+        query = Select(["Name", "City"], [
+            ConditionExecutor("Name", Equal("John"))
+        ])
+        result = query.execute(self.table)
+        # Assert the result based on the condition
+        self.assertEqual(len(result.get_rows()), 1)
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[0].value, "John")
+        self.assertEqual(result.get_column_by_name(
+            "City").elements[0].value, "New York")
+
+    def test_select_age_greater_than_30(self):
+        # Test for selecting rows with Age greater than 30
+        query = Select(["Name", "Age"], [
+            ConditionExecutor("Age", GreaterThan(30))
+        ])
+        result = query.execute(self.table)
+        # Assert the result based on the condition
+        self.assertEqual(len(result.get_rows()), 1)
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[0].value, "Bob")
+        self.assertEqual(result.get_column_by_name(
+            "Age").elements[0].value, 35)
+
+    def test_select_completed_tasks(self):
+        # Test for selecting rows where Completed is True
+        query = Select(["Name", "Completed"], [
+            ConditionExecutor("Completed", Equal(True))
+        ])
+        result = query.execute(self.table)
+        # Assert the result based on the condition
+        self.assertEqual(len(result.get_rows()), 2)
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[0].value, "John")
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[1].value, "Bob")
+        self.assertEqual(result.get_column_by_name(
+            "Completed").elements[0].value, True)
+        self.assertEqual(result.get_column_by_name(
+            "Completed").elements[1].value, True)
+
+    def test_select_age_between_25_and_35(self):
+        # Test for selecting rows where Age is between 25 and 35
+        query = Select(["Name", "Age"], [
+            ConditionExecutor("Age", GreaterThanOrEqualTo(25)),
+            ConditionExecutor("Age", LessThanOrEqualTo(35)),
+        ])
+        result = query.execute(self.table)
+        # Assert the result based on the condition
+        self.assertEqual(len(result.get_rows()), 3)
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[0].value, "John")
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[1].value, "Alice")
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[2].value, "Bob")
+
+    def test_select_city_not_chicago(self):
+        # Test for selecting rows where City is not "Chicago"
+        query = Select(["Name", "City"], [
+            ConditionExecutor("City", NotEqual("Chicago"))
+        ])
+        result = query.execute(self.table)
+        # Assert the result based on the condition
+        self.assertEqual(len(result.get_rows()), 2)
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[0].value, "John")
+        self.assertEqual(result.get_column_by_name(
+            "Name").elements[1].value, "Alice")
 
 
 if __name__ == '__main__':
