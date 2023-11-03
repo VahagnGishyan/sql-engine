5cc2b62bf9a965546b342961e31cfcb91cac09e2
vgishyan <vahagn.gishyan.a@gmail.com>
2023-11-03 23:51:00 +0400

    Revert "feat[config]: updates conan files"
    This reverts commit de2803250b53584697f3a2f430d820c3bbf5b64b.

--- config/conan/run-conan-windows-debug-x64.bat
+++
@@ -1,3 +0,0 @@
-conan install . --output-folder=build --build=missing --profile:build=config/conan/windows-debug-x64 --profile:host=config/conan/windows-debug-x64
-cmake --preset conan-default
-cmake --build build
