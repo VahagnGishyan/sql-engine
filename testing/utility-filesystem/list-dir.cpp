e3f0b4b134458b9f0477d5892e324fbe9b0174dc
vgishyan <vahagn.gishyan.a@gmail.com>
2023-11-03 23:50:05 +0400

    dnw-feat[testing]: adds list-dir

---
+++ testing/utility-filesystem/list-dir.cpp
@@ -0,0 +1,152 @@
+
+#include "utility/filesystem.hpp"
+#include "test-dir-preparation.hpp"
+
+#include <gtest/gtest.h>
+#include <fmt/core.h>
+#include <vector>
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+using namespace SQLEngine::Testing;
+using namespace SQLEngine::Utility;
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+auto AreVectorsEqual(std::vector<std::string> first,
+                     std::vector<std::string> second)
+{
+    // Create copies of the input lists to sort
+    std::sort(first.begin(), first.end());
+    std::sort(second.begin(), second.end());
+
+    // Compare the sorted lists element by element
+    return first == second;
+}
+
+auto VecStrToStrForPrint(const std::vector<std::string> &list)
+{
+    std::string result = "[ ";
+    for (auto &&item : list)
+    {
+        result += "{ " + item + " } ";
+    }
+    result += "]";
+    return result;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+TEST(ListDirTest, ValidDirectory)
+{
+    auto &&testdir = Peparation::GetTestDir();
+    auto &&fileNpath = testdir.GetFileNPath();
+    auto &&dirNpath = testdir.GetDirNPath();
+    auto &&compPath = testdir.GetCompDirPath();
+
+    auto &&fileNinfo = testdir.GetFileNInfo();
+    auto &&dirNinfo = testdir.GetDirNInfo();
+
+    auto &&fileNList = ListDir(fileNpath);
+    ASSERT_TRUE(fileNList != nullptr);
+    ASSERT_EQ(fileNList->size(), fileNinfo->filenameList.size());
+    EXPECT_TRUE(AreVectorsEqual(*fileNList, fileNinfo->filenameList)) << 
+                                fmt::format("first: {}, second: {}", VecStrToStrForPrint(*fileNList), VecStrToStrForPrint(fileNinfo->filenameList));
+
+    auto &&dirNList = ListDir(dirNpath);
+    ASSERT_TRUE(dirNList != nullptr);
+    ASSERT_EQ(dirNList->size(), dirNinfo->dirnameList.size());
+    EXPECT_TRUE(AreVectorsEqual(*dirNList, dirNinfo->dirnameList)) << 
+                                fmt::format("first: {}, second: {}", VecStrToStrForPrint(*fileNList), VecStrToStrForPrint(fileNinfo->filenameList));
+
+    auto &&compList = ListDir(compPath);
+    ASSERT_TRUE(compList != nullptr);
+    EXPECT_EQ(compList->size(), fileNinfo->filenameList.size() + dirNinfo->dirnameList.size()) << 
+                                fmt::format("first: {}, second: {}", VecStrToStrForPrint(*fileNList), VecStrToStrForPrint(fileNinfo->filenameList));
+}
+
+// TEST(ListDirTest, EmptyDirectory)
+// {
+//     auto &&testdir = Peparation::GetTestDir();
+//     auto &&emptydir = testdir.GetEmptyDirPath();
+
+//     auto result = ListDir(emptydir);
+//     ASSERT_TRUE(result != nullptr);
+//     ASSERT_TRUE(result->empty());
+
+//     auto resultfiles = ListDirsInDir(emptydir);
+//     ASSERT_TRUE(resultfiles != nullptr);
+//     ASSERT_TRUE(resultfiles->empty());
+
+//     auto resultdir = ListFilesInDir(emptydir);
+//     ASSERT_TRUE(resultdir != nullptr);
+//     ASSERT_TRUE(resultdir->empty());
+// }
+
+// TEST(ListDirsInDirTest, ValidDirectory)
+// {
+//     auto &&testdir = Peparation::GetTestDir();
+//     auto &&fileNpath = testdir.GetFileNPath();
+//     auto &&dirNpath = testdir.GetDirNPath();
+//     auto &&compPath = testdir.GetCompDirPath();
+
+//     auto &&fileNinfo = testdir.GetFileNInfo();
+//     auto &&dirNinfo = testdir.GetDirNInfo();
+
+//     auto &&fileNList = ListDir(fileNpath);
+//     ASSERT_TRUE(fileNList != nullptr);
+//     EXPECT_EQ(fileNList->size(), 0);
+
+//     auto &&dirNList = ListDir(dirNpath);
+//     ASSERT_TRUE(dirNList != nullptr);
+//     ASSERT_EQ(dirNList->size(), dirNinfo->dirnameList.size());
+//     EXPECT_TRUE(AreVectorsEqual(*dirNList, dirNinfo->dirnameList));
+
+//     auto &&compList = ListDir(compPath);
+//     ASSERT_TRUE(compList != nullptr);
+//     EXPECT_EQ(compList->size(), dirNinfo->dirnameList.size());
+// }
+
+// TEST(ListFilesInDirTest, ValidDirectory)
+// {
+//     auto &&testdir = Peparation::GetTestDir();
+//     auto &&fileNpath = testdir.GetFileNPath();
+//     auto &&dirNpath = testdir.GetDirNPath();
+//     auto &&compPath = testdir.GetCompDirPath();
+
+//     auto &&fileNinfo = testdir.GetFileNInfo();
+//     auto &&dirNinfo = testdir.GetDirNInfo();
+
+//     auto &&fileNList = ListDir(fileNpath);
+//     ASSERT_TRUE(fileNList != nullptr);
+//     ASSERT_EQ(fileNList->size(), fileNinfo->filenameList.size());
+//     EXPECT_TRUE(AreVectorsEqual(*fileNList, fileNinfo->filenameList));
+
+//     auto &&dirNList = ListDir(dirNpath);
+//     ASSERT_TRUE(dirNList != nullptr);
+//     EXPECT_EQ(dirNList->size(), 0);
+
+//     auto &&compList = ListDir(compPath);
+//     ASSERT_TRUE(compList != nullptr);
+//     EXPECT_EQ(compList->size(), fileNinfo->filenameList.size());
+// }
+
+// TEST(ListDirsInDirTest, NonExistentDirectory)
+// {
+//     auto &&testdir = Peparation::GetTestDir();
+//     auto &&nonExisting = testdir.GetNonExistingPath();
+
+//     ASSERT_THROW(ListDir(nonExisting), std::invalid_argument);
+//     ASSERT_THROW(ListDirsInDir(nonExisting), std::invalid_argument);
+//     ASSERT_THROW(ListFilesInDir(nonExisting), std::invalid_argument);
+// }
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
--- testing/utility-filesystem/main.cpp
+++ testing/utility-filesystem/main.cpp
@@ -3,7 +3,28 @@
 //
 //////////////////////////////////////////////////////////////////////////
 
+#include "test-dir-preparation.hpp"
+#include "utility/filesystem.hpp"
 #include <gtest/gtest.h>
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+namespace SQLEngine::Testing::Peparation
+{
+    void SetUp()
+    {
+        auto &&dir = Peparation::GetTestDir();
+    }
+
+    void ClearRemainingTempFiles()
+    {
+        auto &&testdir = Peparation::TestDir::GetTestingWorkDir();
+        auto &&testname = Peparation::TestDir::GetTestingName();
+        SQLEngine::Utility::RemoveDir(testdir + '/' + testname);
+    }
+}
 
 //////////////////////////////////////////////////////////////////////////
 //
@@ -11,6 +32,9 @@
 
 int main(int argc, char **argv)
 {
+    // SQLEngine::Testing::Peparation::ClearRemainingTempFiles();
+    SQLEngine::Testing::Peparation::SetUp();
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
