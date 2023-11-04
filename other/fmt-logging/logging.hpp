ac83ac7c4766fd4fa4128240df5891560c87a328
vgishyan <vahagn.gishyan.a@gmail.com>
2023-11-05 01:15:22 +0400

    feat[feat-testing]: adds fmt-log

---
+++ other/fmt-logging/logging.hpp
@@ -0,0 +1,46 @@
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include <string>
+// #include "utility/defines.hpp"
+#include "log-core.hpp"
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+namespace SQLEngine::Logging
+{
+    // PROJECT_SHARED_EXPORT
+    void Message(const std::string &message);
+    // PROJECT_SHARED_EXPORT
+    void Info(const std::string &message);
+    // PROJECT_SHARED_EXPORT
+    void Signal(const std::string &message);
+    // PROJECT_SHARED_EXPORT
+    void Warning(const std::string &message);
+    // PROJECT_SHARED_EXPORT
+    void Debug(const std::string &message);
+    // PROJECT_SHARED_EXPORT
+    void Error(const std::string &message, const bool dothrow = false);
+
+    // PROJECT_SHARED_EXPORT
+    auto GetMode() -> const Mode;
+    // PROJECT_SHARED_EXPORT
+    void SetMode(const Mode &);
+
+    // PROJECT_SHARED_EXPORT
+    auto GetLogPath() -> const std::string;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
