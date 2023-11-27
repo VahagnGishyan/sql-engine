0cbf152cb0aafaba353ceef62429f8fd631e4bd2
vgishyan <vahagn.gishyan.a@gmail.com>
2023-11-27 23:26:03 +0400

    chore[dev-table]: updates column

--- table/column.cpp
+++ table/column.cpp
@@ -17,21 +17,21 @@ namespace SQLEngine::Table
     //                                                                  //
     //////////////////////////////////////////////////////////////////////
 
-    void Column::Init(const Interface::IColumnInit& init)
-    {
-    }
+    // void Column::Init(const Interface::IColumnInit& init)
+    // {
+    // }
 
-    auto Column::Copy() const -> Interface::UColumn
-    {
-        auto column    = std::make_unique<Column>();
-        column->m_type = m_type;
-        column->m_id   = m_id->Copy();
-        for (auto&& element : m_elements)
-        {
-            column->AddElement(element->Copy());
-        }
-        return column;
-    }
+    // auto Column::Copy() const -> Interface::UColumn
+    // {
+    //     auto column    = std::make_unique<Column>();
+    //     column->m_type = m_type;
+    //     column->m_id   = m_id->Copy();
+    //     for (auto&& element : m_elements)
+    //     {
+    //         column->AddElement(element->Copy());
+    //     }
+    //     return column;
+    // }
 
     //////////////////////////////////////////////////////////////////////
     //                                                                  //
@@ -50,10 +50,6 @@ namespace SQLEngine::Table
         return m_type;
     }
 
-    //////////////////////////////////////////////////////////////////////
-    //                                                                  //
-    //////////////////////////////////////////////////////////////////////
-
     void Column::SetType(const Interface::DynamicType& type)
     {
         m_type = type;
@@ -67,24 +63,24 @@ namespace SQLEngine::Table
     {
         m_elements.push_back(std::move(element));
     }
-    auto Column::GetElement(const Interface::IColumnID& id) -> Interface::UColumnElement
-    {
-    }
-    auto Column::GetElementsIDs(std::function<bool(const Interface::IColumnElement&)> predicate)
-        -> Interface::UColumnIDList
-    {
-    }
+    // auto Column::GetElement(const Interface::IColumnID& id) -> Interface::UColumnElement
+    // {
+    // }
+    // auto Column::GetElementsIDs(std::function<bool(const Interface::IColumnElement&)> predicate)
+    //     -> Interface::UColumnIDList
+    // {
+    // }
 
     //////////////////////////////////////////////////////////////////////
     //                                                                  //
     //////////////////////////////////////////////////////////////////////
 
-    void Column::AddConstraint(Interface::UColumnConstraint constraint)
-    {
-    }
-    void Column::AddConstraints(Interface::UColumnConstraintList element)
-    {
-    }
+    // void Column::AddConstraint(Interface::UColumnConstraint constraint)
+    // {
+    // }
+    // void Column::AddConstraints(Interface::UColumnConstraintList element)
+    // {
+    // }
 
     //////////////////////////////////////////////////////////////////////
     //                                                                  //
@@ -92,13 +88,21 @@ namespace SQLEngine::Table
 
     void Column::RemoveElementsIf(std::function<bool(const Interface::IColumnElement&)> predicate)
     {
-        auto endIt = std::remove_if(m_elements.begin(), m_elements.end(), predicate);
+        auto endIt = std::remove_if(m_elements.begin(), m_elements.end(),
+                                    [&predicate](const Interface::UColumnElement& ptritem)
+                                    {
+                                        return predicate(*ptritem);
+                                    });
         // Remove all even numbers collected in the end of vector
         m_elements.erase(endIt, m_elements.end());
     }
     void Column::UpdateElementsIf(std::function<void(Interface::IColumnElement&)> modifier)
     {
-        std::for_each(m_elements.begin(), m_elements.end(), modifier);
+        std::for_each(m_elements.begin(), m_elements.end(),
+                      [&modifier](Interface::UColumnElement& ptritem)
+                      {
+                          modifier(*ptritem);
+                      });
     }
 
     //////////////////////////////////////////////////////////////////////
--- table/column.hpp
+++ table/column.hpp
@@ -29,26 +29,24 @@ namespace SQLEngine::Table
         using ColumnElementList = std::vector<Interface::UColumnElement>;
 
        public:
-        void Init(const Interface::IColumnInit& init) override;
-        auto Copy() const -> Interface::UColumn override;
+        // void Init(const Interface::IColumnInit& init) override;
+        // auto Copy() const -> Interface::UColumn override;
 
        public:
         auto GetID() const -> const Interface::IColumnID& override;
         auto GetSize() const -> unsigned int override;
         auto GetType() const -> const Interface::DynamicType override;
-
-       public:
         void SetType(const Interface::DynamicType& type) override;
 
        public:
         void AddElement(Interface::UColumnElement element) override;
-        auto GetElement(const Interface::IColumnID& id) -> Interface::UColumnElement override;
-        auto GetElementsIDs(std::function<bool(const Interface::IColumnElement&)> predicate)
-            -> Interface::UColumnIDList override;
+        // auto GetElement(const Interface::IColumnID& id) -> Interface::UColumnElement override;
+        // auto GetElementsIDs(std::function<bool(const Interface::IColumnElement&)> predicate)
+        //     -> Interface::UColumnIDList override;
 
-       public:
-        void AddConstraint(Interface::UColumnConstraint constraint) override;
-        void AddConstraints(Interface::UColumnConstraintList element) override;
+    //    public:
+    //     void AddConstraint(Interface::UColumnConstraint constraint) override;
+    //     void AddConstraints(Interface::UColumnConstraintList element) override;
 
        public:
         void RemoveElementsIf(std::function<bool(const Interface::IColumnElement&)> predicate) override;
