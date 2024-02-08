494c0a019bb04c7e7d836a73d3403f319fd5ce8a
vgishyan <vahagn.gishyan.a@gmail.com>
2024-02-08 17:17:04 +0400

    fix[config]: updates conan linux

--- config/conan/linux/run-conan-linux-debug.sh
+++ config/conan/linux/run-conan-linux-debug.sh
@@ -1,3 +1,3 @@
-conan install .conanfile-linux.txt --output-folder=build --build=missing --profile:build=config/conan/linux/linux-debug-x64 --profile:host=config/conan/linux/linux-debug-x64
-# cmake --preset conan-debug
+conan install ./conanfile-linux.txt --output-folder=build --build=missing --profile:build=config/conan/linux/linux-debug-x64 --profile:host=config/conan/linux/linux-debug-x64
+cmake --preset conan-debug
 cmake --build build
