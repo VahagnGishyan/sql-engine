198ee47e295e96a048065ef7f450d4e41a1082e3
vgishyan <vahagn.gishyan.a@gmail.com>
2024-02-08 16:28:05 +0400

    feat[config]: adds flex bison

--- config/conan/windows/run-conan-windows-debug.bat
+++ config/conan/windows/run-conan-windows-debug.bat
@@ -1,4 +1,4 @@
 
-conan install . --build=missing --profile:build=config/conan/windows/windows-debug-x64 --profile:host=config/conan/windows/windows-debug-x64
+conan install  ./conanfile-windows.txt --build=missing --profile:build=config/conan/windows/windows-debug-x64 --profile:host=config/conan/windows/windows-debug-x64
 cmake --preset conan-default
 cmake --build build
